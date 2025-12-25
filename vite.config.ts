41c87f77cd223c56558e8d951e653268795c9fa9
Jacob Kostecki <jacob@kostecki.dk>
2025-12-25 19:59:19 +0100

    add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -14,6 +15,8 @@ export default defineConfig({
     tanstackStart({
       srcDirectory: "src",
     }),
+    nitro(),
     viteReact(),
   ],
+  nitro: {},
 });
