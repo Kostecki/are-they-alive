5a266dd0be4d88ac78e7fae79f255bbee3137e8e
Jacob Kostecki <jacob@kostecki.dk>
2025-12-30 01:40:17 +0100

    styling front page link

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,20 @@
+import { Anchor, type AnchorProps } from "@mantine/core";
+import { createLink, type LinkComponent } from "@tanstack/react-router";
+import * as React from "react";
+
+interface MantineAnchorProps extends Omit<AnchorProps, "href"> {}
+
+const MantineLinkComponent = React.forwardRef<
+	HTMLAnchorElement,
+	MantineAnchorProps
+>((props, ref) => {
+	return <Anchor ref={ref} {...props} />;
+});
+
+const CreatedLinkComponent = createLink(MantineLinkComponent);
+
+export const CustomLink: LinkComponent<typeof MantineLinkComponent> = (
+	props,
+) => {
+	return <CreatedLinkComponent preload="intent" {...props} />;
+};
--- src/components/HomePageLayout.tsx
+++ src/components/HomePageLayout.tsx
@@ -1,5 +1,6 @@
 import { Grid, Text, Title } from "@mantine/core";
-import { Link } from "@tanstack/react-router";
+
+import { CustomLink } from "./CustomLink";
 
 type InputProps = {
 	children: React.ReactNode;
@@ -9,11 +10,12 @@ export default function HomePageLayout({ children }: InputProps) {
 	return (
 		<Grid justify="center" pt="50">
 			<Grid.Col span={10}>
-				<Link to="/">
+				<CustomLink to="/" underline="never" c="dimmed">
 					<Title order={1} ta="center">
 						Are They Alive?
 					</Title>
-				</Link>
+				</CustomLink>
+
 				<Text c="dimmed" fs="italic" size="sm" ta="center">
 					Find out how life treated the cast of your favorite films and shows.
 				</Text>
