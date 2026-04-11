7925b76b3164c583e62d8733d450f7e6063e1eb6
Jacob Kostecki <jacob@kostecki.dk>
2026-04-11 21:10:44 +0200

    chore: generate featured from top rated

--- src/utils/tmdb.ts
+++ src/utils/tmdb.ts
@@ -3,11 +3,13 @@ import { TMDB } from "tmdb-ts";
 let tmdbInstance: TMDB | null = null;
 
 export function getTMDB(): TMDB {
-  if (!tmdbInstance) {
-    if (!process.env.TMDB_API_KEY) {
-      throw new Error("TMDB API key is missing in environment variables.");
-    }
-    tmdbInstance = new TMDB(process.env.TMDB_API_KEY);
-  }
-  return tmdbInstance;
+	if (!tmdbInstance) {
+		if (!process.env.TMDB_API_TOKEN) {
+			throw new Error(
+				"TMDB Read Access token is missing in environment variables.",
+			);
+		}
+		tmdbInstance = new TMDB(process.env.TMDB_API_TOKEN);
+	}
+	return tmdbInstance;
 }
